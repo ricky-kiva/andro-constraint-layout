3ef05d12de4e5e6d527935a5d8288dab0b43311a
ricky-kiva <ricky.kiva@gmail.com>
2023-03-05 20:12:44 +0700

    apply Orange theme

--- app/src/main/java/com/rickyslash/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/rickyslash/constraintlayout/MainActivity.kt
@@ -9,3 +9,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+/* Android Design Guideline Principal */
+// - Show only 'important' information
+// - If an app asks 'user permission', there should be a mechanism to 'deny' that permission
+// - 'interrupt' if needed
+// - Use 'concise' text
+// - 'Protect' user data
+// - Allow user do things 'fast' in 'easy way'
+// - If looks the 'same', 'behavior' need to be the 'same'
+// - 'Help' user to 'make decision', but 'let' 'user' 'decide' it
+
+/* App Development Best Practice */
+// - Good performance: tips, if a process need long time, then run it in background / async-ly
+// - Responsive Design: give best user experience
+// - Accomodate user's information needs
+// - Battery Optimization
+// - Design network connection efficiently
